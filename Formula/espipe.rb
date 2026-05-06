b6d597272101761ddb9df2e1a8b255dae26b72a3
Ryan Eno <ryan.eno@elastic.co>
2026-05-06 00:16:42 -0700

    Update espipe formula to 0.4.0

--- Formula/espipe.rb
+++ Formula/espipe.rb
@@ -1,9 +1,9 @@
 class Espipe < Formula
   desc "Pipe NDJSON, JSON, or CSV documents into Elasticsearch"
   homepage "https://github.com/VimCommando/espipe"
-  url "https://github.com/VimCommando/espipe/archive/refs/tags/v0.3.0.tar.gz"
-  version "0.3.0"
-  sha256 "4fc8a064ead2af190551460fb595983e41c7d95ad0258213fb19226449799470"
+  url "https://github.com/VimCommando/espipe/archive/refs/tags/v0.4.0.tar.gz"
+  version "0.4.0"
+  sha256 "632f556e535fb6c6e1bae2c6116e7396697aea4b0cfd303f3b2434beb371fef0"
   license "Apache-2.0"
 
   depends_on "rust" => :build
